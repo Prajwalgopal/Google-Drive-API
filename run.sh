22c51b01a92f63fb8c65cd704ab88f31bdb691ae
Prajwal Gopal <55605535+Prajwalgopal@users.noreply.github.com>
2020-08-06 17:47:09 +0100

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,8 @@
+export FN_AUTH_REDIRECT_URI=http://localhost:8040/google/auth
+export FN_BASE_URI=http://localhost:8040
+export FN_CLIENT_ID="705692891853-c39os4jn6mvkqokbiq3oi53rin4cge9f.apps.googleusercontent.com"
+export FN_CLIENT_SECRET="Zxb07O8jpyp1ahA-N7jYd_Wt"
+export FLASK_APP=app.py
+export FLASK_DEBUG=1
+export FN_FLASK_SECRET_KEY=Rio123
+python -m flask run -p 8040
